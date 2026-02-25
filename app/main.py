7b62ccfbd42a19b03fae321cd90dc235a1d8f1f5
Vijay Chand <vijaychand6979@googlemail.com>
2026-02-25 11:50:00 +0000

    updated as per ai suggestions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -9,48 +12,47 @@ class Distance:
         return f"Distance(km={self.km})"
 
     @staticmethod
-    def get_raw_value(distance: object) -> int:
+    def get_raw_value(distance: Distance | int | float) -> int | float:
         if isinstance(distance, Distance):
             return distance.km
-        else:
-            return distance
+        return distance
 
-    def __add__(self, distance: object) -> object:
+    def __add__(self, distance: Distance | int | float) -> Distance:
         value1 = Distance.get_raw_value(self)
         value2 = Distance.get_raw_value(distance)
         return Distance(value1 + value2)
 
-    def __iadd__(self, distance: object) -> object:
+    def __iadd__(self, distance: Distance | int | float) -> Distance:
         self.km += Distance.get_raw_value(distance)
         return self
 
-    def __mul__(self, amount: int) -> object:
+    def __mul__(self, amount: int | float) -> Distance:
         return Distance(self.km * amount)
 
-    def __truediv__(self, amount: int) -> object:
+    def __truediv__(self, amount: int | float) -> Distance:
         return Distance(round(self.km / amount, 2))
 
-    def __lt__(self, distance: object) -> bool:
+    def __lt__(self, distance: Distance | int | float) -> bool:
         return (
             Distance.get_raw_value(self) < Distance.get_raw_value(distance)
         )
 
-    def __gt__(self, distance: object) -> bool:
+    def __gt__(self, distance: Distance | int | float) -> bool:
         return (
             Distance.get_raw_value(self) > Distance.get_raw_value(distance)
         )
 
-    def __eq__(self, distance: object) -> bool:
+    def __eq__(self, distance: Distance | int | float) -> bool:
         return (
             Distance.get_raw_value(self) == Distance.get_raw_value(distance)
         )
 
-    def __le__(self, distance: object) -> bool:
+    def __le__(self, distance: Distance | int | float) -> bool:
         return (
             Distance.get_raw_value(self) <= Distance.get_raw_value(distance)
         )
 
-    def __ge__(self, distance: object) -> bool:
+    def __ge__(self, distance: Distance | int | float) -> bool:
         return (
             Distance.get_raw_value(self) >= Distance.get_raw_value(distance)
         )
